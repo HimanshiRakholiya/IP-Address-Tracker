f63e680a8abe3aa073c9e38e488e01ec1b6c8a94
HimanshiRakholiya <146310758+HimanshiRakholiya@users.noreply.github.com>
2025-07-28 02:05:53 +0530

    Update Map.js

--- src/component/Map.js
+++ src/component/Map.js
@@ -2,18 +2,24 @@ import React, { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import styled from "styled-components";
-import { Icon } from "leaflet";
+import L from "leaflet";
 
 const Map = ({ coordinates }) => {
-  const mapIcon = new Icon({
-    iconUrl: "/path/to/icon.png", 
-    iconSize: [32, 32],
+  const mapIcon = new L.Icon({
+    iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41], 
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    shadowSize: [41, 41],
   });
 
   const MapUpdater = ({ coordinates }) => {
     const map = useMap();
     useEffect(() => {
-      map.setView([coordinates.lat, coordinates.lng], 13);
+      if (coordinates?.lat && coordinates?.lng) {
+        map.setView([coordinates.lat, coordinates.lng], 13);
+      }
     }, [coordinates, map]);
 
     return null;
@@ -25,9 +31,10 @@ const Map = ({ coordinates }) => {
         center={[coordinates.lat, coordinates.lng]}
         zoom={13}
         className="map-container"
+        scrollWheelZoom={true}
       >
         <TileLayer
-          attribution="Google Maps"
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[coordinates.lat, coordinates.lng]} icon={mapIcon} />
